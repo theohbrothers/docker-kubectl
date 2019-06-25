138aed32236eb2f4ddf675ca5b0a66036f24fc6e
Leo <leonardjonathanoh@live.com>
2019-06-25 09:28:51 +0000

    Update readme

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 [![pipeline status](https://gitlab.com/leojonathanoh/docker-kubectl/badges/dev/pipeline.svg)](https://gitlab.com/leojonathanoh/docker-kubectl/commits/dev)
 
+Dockerized `kubectl` image with some optional tools
+
 | Tags |
 |:-------:| 
 | `:envsubst` | 
--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -3,6 +3,8 @@
 
 [![pipeline status](https://gitlab.com/leojonathanoh/docker-kubectl/badges/dev/pipeline.svg)](https://gitlab.com/leojonathanoh/docker-kubectl/commits/dev)
 
+Dockerized ``kubectl`` image with some optional tools
+
 | Tags |
 |:-------:| $( $VARIANTS | % {
 "`n| ``:$( $_['tag'] )`` |"
