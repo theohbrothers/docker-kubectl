6e7bb7aa9fd4595a31063ba91ba90cd2dec27d7f
Leo <leonardjonathanoh@live.com>
2019-07-16 16:49:36 +0000

    Bump kustomize to v3.0.2

--- generate/templates/Dockerfile/alpine/kustomize/kustomize.ps1
+++ generate/templates/Dockerfile/alpine/kustomize/kustomize.ps1
@@ -1,6 +1,6 @@
 @'
 RUN apk add --no-cache curl \
-    && curl -L https://github.com/kubernetes-sigs/kustomize/releases/download/v3.0.0-pre/kustomize_3.0.0-pre_linux_amd64 -o /usr/local/bin/kustomize \
+    && curl -L https://github.com/kubernetes-sigs/kustomize/releases/download/v3.0.2/kustomize_3.0.2_linux_amd64 -o /usr/local/bin/kustomize \
     && chmod +x /usr/local/bin/kustomize \
     && apk del curl
 '@
