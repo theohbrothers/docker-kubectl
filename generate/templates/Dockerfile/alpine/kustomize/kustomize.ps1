21065855693346453099d794d19c5f8ed7fa7cfe
Leo <leonardjonathanoh@live.com>
2019-08-05 22:20:48 +0000

    Revert kustomize to v2.0.3

--- generate/templates/Dockerfile/alpine/kustomize/kustomize.ps1
+++ generate/templates/Dockerfile/alpine/kustomize/kustomize.ps1
@@ -1,6 +1,6 @@
 @'
 RUN apk add --no-cache curl \
-    && curl -L https://github.com/kubernetes-sigs/kustomize/releases/download/v3.0.3/kustomize_3.0.3_linux_amd64 -o /usr/local/bin/kustomize \
+    && curl -L https://github.com/kubernetes-sigs/kustomize/releases/download/v2.0.3/kustomize_2.0.3_linux_amd64 -o /usr/local/bin/kustomize \
     && chmod +x /usr/local/bin/kustomize \
     && apk del curl
 '@
