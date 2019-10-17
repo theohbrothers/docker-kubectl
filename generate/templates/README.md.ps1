0e290712fd6066473cb57a371b0ac6da24e96c56
Leo <leonardjonathanoh@live.com>
2019-10-17 12:39:24 +0000

    Update readme with badges: gitlab-ci, github-tag, docker-image-size, docker-image-layers

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -1,7 +1,10 @@
 @"
 # docker-kubectl
 
-[![pipeline status](https://gitlab.com/leojonathanoh/docker-kubectl/badges/dev/pipeline.svg)](https://gitlab.com/leojonathanoh/docker-kubectl/commits/dev)
+[![gitlab-ci](https://img.shields.io/gitlab/pipeline/leojonathanoh/docker-kubectl/dev)](https://gitlab.com/leojonathanoh/docker-kubectl/commits/dev)
+<!-- [![github-tag](https://img.shields.io/github/tag/leojonathanoh/docker-kubectl)](https://github.com/leojonathanoh/docker-kubectl/releases/) -->
+[![docker-image-size](https://img.shields.io/microbadger/image-size/leojonathanoh/docker-kubectl/latest)](https://hub.docker.com/r/leojonathanoh/docker-kubectl)
+[![docker-image-size](https://img.shields.io/microbadger/image-size/leojonathanoh/docker-kubectl/latest)](https://hub.docker.com/r/leojonathanoh/docker-kubectl)
 
 Dockerized ``kubectl`` alpine image with some optional tools
 
