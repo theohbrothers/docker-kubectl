06624c552ea7b313496312ff1b099a1b1197e0b9
Leo <leonardjonathanoh@live.com>
2019-10-23 05:23:19 +0000

    Update readme badges

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -2,9 +2,8 @@
 # docker-kubectl
 
 [![gitlab-ci](https://img.shields.io/gitlab/pipeline/leojonathanoh/docker-kubectl/dev)](https://gitlab.com/leojonathanoh/docker-kubectl/commits/dev)
-<!-- [![github-tag](https://img.shields.io/github/tag/leojonathanoh/docker-kubectl)](https://github.com/leojonathanoh/docker-kubectl/releases/) -->
 [![docker-image-size](https://img.shields.io/microbadger/image-size/leojonathanoh/docker-kubectl/latest)](https://hub.docker.com/r/leojonathanoh/docker-kubectl)
-[![docker-image-size](https://img.shields.io/microbadger/image-size/leojonathanoh/docker-kubectl/latest)](https://hub.docker.com/r/leojonathanoh/docker-kubectl)
+[![docker-image-layers](https://img.shields.io/microbadger/layers/leojonathanoh/docker-kubectl/latest)](https://hub.docker.com/r/leojonathanoh/docker-kubectl)
 
 Dockerized ``kubectl`` alpine image with some optional tools
 
