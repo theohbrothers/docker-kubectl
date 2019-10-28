b156bef34510e91c519a2f1a94a8f30f0be36a15
Leo <leonardjonathanoh@live.com>
2019-10-28 18:53:42 +0000

    Rename repository from leojonathanoh/docker-kubectl to theohbrothers/docker-kubectl

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -1,9 +1,9 @@
 @"
 # docker-kubectl
 
-[![gitlab-ci](https://img.shields.io/gitlab/pipeline/leojonathanoh/docker-kubectl/dev)](https://gitlab.com/leojonathanoh/docker-kubectl/commits/dev)
-[![docker-image-size](https://img.shields.io/microbadger/image-size/leojonathanoh/docker-kubectl/latest)](https://hub.docker.com/r/leojonathanoh/docker-kubectl)
-[![docker-image-layers](https://img.shields.io/microbadger/layers/leojonathanoh/docker-kubectl/latest)](https://hub.docker.com/r/leojonathanoh/docker-kubectl)
+[![gitlab-ci](https://img.shields.io/gitlab/pipeline/theohbrothers/docker-kubectl/dev)](https://gitlab.com/theohbrothers/docker-kubectl/commits/dev)
+[![docker-image-size](https://img.shields.io/microbadger/image-size/theohbrothers/docker-kubectl/latest)](https://hub.docker.com/r/theohbrothers/docker-kubectl)
+[![docker-image-layers](https://img.shields.io/microbadger/layers/theohbrothers/docker-kubectl/latest)](https://hub.docker.com/r/theohbrothers/docker-kubectl)
 
 Dockerized ``kubectl`` alpine image with some optional tools
 
