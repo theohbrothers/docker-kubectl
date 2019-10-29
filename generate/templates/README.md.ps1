3fff73690f96fb0cfd4764820f4a7b8e9d61f4ad
Leo <leonardjonathanoh@live.com>
2019-10-29 21:42:26 +0000

    Update readme with github-actions badge

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -1,7 +1,7 @@
 @"
 # docker-kubectl
 
-[![gitlab-ci](https://img.shields.io/gitlab/pipeline/theohbrothers/docker-kubectl/dev)](https://gitlab.com/theohbrothers/docker-kubectl/commits/dev)
+[![github-actions](https://github.com/theohbrothers/docker-kubectl/workflows/build/badge.svg)](https://github.com/theohbrothers/docker-kubectl/actions)
 [![docker-image-size](https://img.shields.io/microbadger/image-size/theohbrothers/docker-kubectl/latest)](https://hub.docker.com/r/theohbrothers/docker-kubectl)
 [![docker-image-layers](https://img.shields.io/microbadger/layers/theohbrothers/docker-kubectl/latest)](https://hub.docker.com/r/theohbrothers/docker-kubectl)
 
