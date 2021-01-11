7c0bfd9f32c5c2cf17199c681c4292e2719006e7
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-01-11 07:51:01 +0000

    Fix: Fix github actions badge in readme

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -1,7 +1,7 @@
 @"
 # docker-kubectl
 
-[![github-actions](https://github.com/theohbrothers/docker-kubectl/workflows/build/badge.svg)](https://github.com/theohbrothers/docker-kubectl/actions)
+[![github-actions](https://github.com/theohbrothers/docker-kubectl/workflows/ci-master-pr/badge.svg)](https://github.com/theohbrothers/docker-kubectl/actions)
 [![docker-image-size](https://img.shields.io/microbadger/image-size/theohbrothers/docker-kubectl/latest)](https://hub.docker.com/r/theohbrothers/docker-kubectl)
 [![docker-image-layers](https://img.shields.io/microbadger/layers/theohbrothers/docker-kubectl/latest)](https://hub.docker.com/r/theohbrothers/docker-kubectl)
 
