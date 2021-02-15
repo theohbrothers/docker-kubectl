cd05fe9a944c30ee3b58c0666bb3157bd33da2b4
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-02-15 19:22:53 +0000

    Docs: Add tags section header in readme

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -7,18 +7,20 @@
 
 Dockerized ``kubectl`` alpine image with useful tools.
 
-| Tags |
-|:-------:|
+## Tags
+
+| Tag | Dockerfile Build Context |
+|:-------:|:---------:|
 $(
 ($VARIANTS | % {
     if ( $_['tag_as_latest'] ) {
 @"
-| ``:$( $_['tag'] )``, ``:latest`` |
+| ``:$( $_['tag'] )``, ``:latest`` | [View](variants/$( $_['tag'] ) ) |
 
 "@
     }else {
 @"
-| ``:$( $_['tag'] )`` |
+| ``:$( $_['tag'] )`` | [View](variants/$( $_['tag'] ) ) |
 
 "@
     }
