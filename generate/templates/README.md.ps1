f042ee47c4204d3ebabd0180baad9ac4e18fd366
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-09-11 16:27:55 +0000

    Docs (readme): Replace tag badge with release badge

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -2,7 +2,7 @@
 # docker-kubectl
 
 [![github-actions](https://github.com/theohbrothers/docker-kubectl/workflows/ci-master-pr/badge.svg)](https://github.com/theohbrothers/docker-kubectl/actions)
-[![github-tag](https://img.shields.io/github/tag/theohbrothers/docker-kubectl)](https://github.com/theohbrothers/docker-kubectl/releases/)
+[![github-release](https://img.shields.io/github/v/release/theohbrothers/docker-kubectl?style=flat-square)](https://github.com/theohbrothers/docker-kubectl/releases/)
 [![docker-image-size](https://img.shields.io/docker/image-size/theohbrothers/docker-kubectl/latest)](https://hub.docker.com/r/theohbrothers/docker-kubectl)
 
 Dockerized ``kubectl`` with useful tools.
