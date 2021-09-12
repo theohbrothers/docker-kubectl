6008d729fd3be814547cc554764541d206076c5c
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-09-12 16:11:27 +0000

    Refactor (generate): Remove Dockerfile.header.ps1 and Dockerfile.footer.ps1 and only use Dockerfile.ps1

--- generate/templates/Dockerfile.ps1
+++ generate/templates/Dockerfile.ps1
@@ -1,6 +1,19 @@
 @"
-$(
-($VARIANT['_metadata']['components'] | % {
+FROM $( $VARIANT['_metadata']['distro'] ):$( $VARIANT['_metadata']['distro_version'] )
+ARG TARGETPLATFORM
+ARG BUILDPLATFORM
+RUN echo "I am running on `$BUILDPLATFORM, building for `$TARGETPLATFORM"
+
+# kubectl
+RUN apk add --no-cache curl \
+    && curl -L https://storage.googleapis.com/kubernetes-release/release/$( $VARIANT['_metadata']['package_version'] )/bin/`$TARGETPLATFORM/kubectl -o /usr/local/bin/kubectl \
+    && chmod +x /usr/local/bin/kubectl \
+    && apk del curl
+
+
+"@
+
+$VARIANT['_metadata']['components'] | % {
     $component = $_
 
     switch( $component ) {
@@ -107,6 +120,9 @@ RUN apk add --no-cache --virtual .build-dependencies $PHPIZE_DEPS \
             throw "No such component: $component"
         }
     }
-}) -join ''
-)
+}
+
+@"
+ENTRYPOINT [ "/usr/local/bin/kubectl" ]
+
 "@
